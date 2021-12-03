145700d481f9a1ee792c6705f43be96582ec6ee0
Brett Rowberry <brett.rowberry@gmail.com>
2021-12-03 13:45:32 -0500

    reduce version and check

--- one.clj
+++ one.clj
@@ -12,7 +12,7 @@
    260
    263])
 
-(defn count-depth-increases
+(defn count-depth-increases-loop
   [depths]
   (loop [depths depths
        sum 0]
@@ -23,4 +23,22 @@
       (recur (next depths) new-sum))
     sum)))
 
-(count-depth-increases depths)
+(defn count-depth-increases-reduce
+  [depths]
+  (second
+   (reduce
+    (fn [[remaining sum] current]
+      (if (seq remaining)
+        (if (> (first remaining) current)
+          [(rest remaining) (inc sum)]
+          [(rest remaining) sum])
+        [[] sum]))
+    [(rest depths) 0]
+    depths)))
+
+(comment
+  (every?
+   #(= 7 (% depths))
+   [count-depth-increases-loop
+    count-depth-increases-reduce])
+  )
