9cff2aab0f34f8d51b601b6af2537de2a4f5d630
Brett Rowberry <brett.rowberry@gmail.com>
2021-12-03 17:18:30 -0500

    keywordize and part 2

--- two.clj
+++ two.clj
@@ -12,28 +12,50 @@ forward 2")
 (defn parse-command
   [s]
   (let [[command magnitude] (re-seq #"\w+" s)]
-       [command (Integer/parseInt magnitude)]))
+       [(keyword command) (Integer/parseInt magnitude)]))
 
 (defn parse-commands
   [commands-string]
   (let [lines (string/split-lines commands-string)]
     (map parse-command lines)))
 
-(defn next-position
+(defn next-position-part-1
   [[x y] [command magnitude]]
   (case command
-    "forward" [(+ x magnitude) y]
-    "down"    [x (+ y magnitude)]
-    "up"      [x (- y magnitude)]))
+    :forward [(+ x magnitude) y]
+    :down    [x (+ y magnitude)]
+    :up      [x (- y magnitude)]))
 
-(defn final-position
+(defn final-position-part-1
   [commands-string]
   (let [commands (parse-commands commands-string)
         [x y] (reduce
                         (fn [position command]
-                          (next-position position command))
+                          (next-position-part-1 position command))
                         [0 0]
                         commands)]
     (* x y)))
 
-(final-position commands-string)
+(final-position-part-1 commands-string)
+
+;;; Part 2
+(defn next-position-part-2
+  [[x y aim] [command magnitude]]
+  (case command
+    :forward [(+ x magnitude) (+ y (* aim magnitude)) aim]
+    :down    [x y (+ aim magnitude)]
+    :up      [x y (- aim magnitude)]))
+
+(defn final-position-part-2
+  [commands-string]
+  (let [commands (parse-commands commands-string)
+        [x y] (reduce
+               (fn [position command]
+                 (next-position-part-2 position command))
+               [0 0 0]
+               commands)]
+    (* x y)))
+
+(comment
+  (= 900
+     (final-position-part-2 commands-string)))
